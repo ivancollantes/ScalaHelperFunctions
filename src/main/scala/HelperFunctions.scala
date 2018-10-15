d34ace6d7717621eafbd8a387e3762cb106bbc95
Ivan Collantes <icollantes@airlan.es>
2018-10-15 13:19:40 +0200

    added functions

--- src/main/scala/HelperFunctions.scala
+++ src/main/scala/HelperFunctions.scala
@@ -1,6 +1,6 @@
 object HelperFunctions {
 
-  /**
+  /*
    * foldLeft takes 2 arguments:
        ** an initial value (in this case an empty list declared as "List[Int]()") for the result and
        ** a pre-defined combining operation that, again, takes 2 arguments:
@@ -10,4 +10,10 @@ object HelperFunctions {
        ** "element :: result" returns result with element appended at the beginning
    */
   def reverseList(arr: List[Int]): List[Int] = arr.foldLeft(List[Int]()) { (result, element) => element :: result }
+
+  def sumOdd(arr: List[Int]): Int = arr.filter(_ % 2 != 0).sum
+
+  def countNumberOfElements(arr: List[Int]): Int = arr.foldLeft(0) { (count, _) => count + 1}
+
+  def getAbsoluteValues(arr: List[Int]): List[Int] = arr.map(_.abs)
 }
